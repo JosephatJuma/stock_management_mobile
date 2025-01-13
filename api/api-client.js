ab7b173b090e4ea15b4dcf1534da5d1442fc1cf4
Juma Josephat <91407741+JosephatJuma@users.noreply.github.com>
2025-01-13 22:50:26 +0300

    add api client

---
+++ api/api-client.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { API_URL, API_KEY } from "./api-constants";
+export default class ApiClient {
+  constructor() {
+    this.base_url = API_URL;
+    this.api_key = API_KEY;
+  }
+
+  //Resubale methods
+  async _makeRequest(method, endpoint, data) {
+    //const tokens = store.getState().tokens.tokens || "None";
+    try {
+      const response = await axios({
+        method: method,
+        url: `${this.base_url}/${endpoint}`,
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-KEY": this.api_key,
+          //Authorization: `Bearer ${tokens?.access_token}`,
+        },
+        withCredentials: true,
+        data: data,
+      });
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
+  async _submitFile(method, endpoint, data) {
+    //const tokens = store.getState().tokens.tokens || "None";
+    try {
+      const response = await axios({
+        method: method,
+        url: `${this.base_url}/${endpoint}`.replace("localhost", "127.0.0.1"),
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "X-API-KEY": this.api_key,
+          //Authorization: `Bearer ${tokens?.access_token}`,
+        },
+        withCredentials: true,
+        data: data,
+      });
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
+}
